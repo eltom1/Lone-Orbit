0922546992eccf25dfd254183a708e5bfa29a35c
eltom1 <tomasdellosso4@gmail.com>
2026-05-15 16:19:55 -0300

    m

--- jugador.gd
+++ jugador.gd
@@ -8,9 +8,11 @@ const FUERZA_SALTO = 8.0
 
 @onready var anim = $UAL1_Standard/AnimationPlayer
 
+var en_gravedad = true
+
 func _physics_process(delta):
 	var hud = get_tree().get_first_node_in_group("hud")
-	print("hud: ", hud, " estamina: ", hud.estamina_actual if hud else "NULL")
+	#print("hud: ", hud, " estamina: ", hud.estamina_actual if hud else "NULL")
 	var tiene_estamina = true if hud == null else hud.estamina_actual > 0
 	var corriendo = Input.is_action_pressed("SHIFT") and tiene_estamina
 	var agachado = Input.is_action_pressed("CTRL") and is_on_floor()
@@ -26,7 +28,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, vel_objetivo)
 
 	if not is_on_floor():
-		velocity.y += GRAVEDAD * delta
+		if en_gravedad:
+			velocity.y += GRAVEDAD * delta
+		else:
+			velocity.y = move_toward(velocity.y, 0, 5 * delta)
 
 	if Input.is_action_just_pressed("SPACE") and is_on_floor() and not agachado:
 		velocity.y = FUERZA_SALTO
